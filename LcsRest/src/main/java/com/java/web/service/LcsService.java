fad34994c4d0babd2795d425dfd55b59e89c1dbb
srikanth052 <40168789+srikanth052@users.noreply.github.com>
2018-06-11 07:22:39 -0400

    Add files via upload

---
+++ LcsRest/src/main/java/com/java/web/service/LcsService.java
@@ -0,0 +1,12 @@
+package com.java.web.service;
+
+import java.util.List;
+import java.util.Set;
+
+import com.java.web.model.LCSDto;
+
+public interface LcsService {
+
+public List<LCSDto> findLcs(List<String> lcsDto);
+
+}
